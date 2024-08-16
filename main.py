5218af11afac032bfa39fc607242568719b2beb7
g_nix <thedrawnator@gmail.com>
2024-08-15 23:07:14 -0300

    [FIX] implementation of the efined attributes for request

--- main.py
+++ main.py
@@ -12,14 +12,20 @@ emailsearch = {}
 users = {}
 itens = {}
 
-def save_user(resp,name,password,phone,email,city):
-    new_id = len(users)
-    new_user = Person(new_id,name,password,phone,email,city)
-    users[new_id] = new_user
-    emailsearch[email] = new_id
-    resp.set_cookie('email', name)
-    resp.set_cookie('password',password)
-    return resp
+def save_item(name,owner_id,desc,photo,available):
+    item_id = len(itens)
+    item_instance = Item(item_id,name,owner_id,desc,photo,available)
+    itens[item_instance.id] = item_instance
+    users[item_instance.owner_id].itens[item_instance.id] = item_instance
+    return item_id
+    
+
+def save_user(name,password,phone,email,city,resp=None):
+    user_id = len(users)
+    new_user = Person(user_id,name,password,phone,email,city)
+    users[user_id] = new_user
+    emailsearch[email] = user_id
+    return user_id
 
 def save_cookies(resp,email,password):
     resp.set_cookie('email', email)
@@ -80,8 +86,9 @@ def confirmation():
         email = request.form['email']
         city = request.form['city']
         if email not in emailsearch:
+            save_user(name,password,phone,email,city)
             resp = make_response(redirect(url_for('menu')))
-            save_user(resp,name,password,phone,email,city)
+            resp = save_cookies(resp,email,password)
             return resp
         else:
             output = "user already registered with this email" 
@@ -133,19 +140,19 @@ def item_request():
     item_id = int(request.form["id"])
     user = users[emailsearch[email]]
     seller = users[itens[item_id].owner_id]
-    new_request = Request("open",item_id,name,user.phone,seller.phone)
+    new_request = Request("open",item_id,user.id,seller.id)
     user.requests_made[item_id] = new_request
-    seller.received_requests[(item_id,name)] = new_request
+    seller.received_requests[(item_id,user.id)] = new_request
     return redirect(url_for("open_requests"))
 
 @app.route('/accept_request',methods=['GET','POST'])
-def accept():
+def accept_request():
     email, err = check_user()
     if err:
         return render_template('login.html')
     item_id = int(request.form["id"])
     user = users[emailsearch[email]]
-    interested = request.form["interested"]
+    interested = int(request.form["interested"])
     user.received_requests[(item_id,interested)].state = 'accepted'
     return redirect(url_for("open_received_requests"))
 
@@ -197,7 +204,7 @@ def open_received_requests():
     if err:
         return render_template('login.html')
     user = users[emailsearch[email]]
-    return render_template('open_received_requests.html',requests=user.received_requests,itens=itens)
+    return render_template('open_received_requests.html',requests=user.received_requests,itens=itens,users=users)
 
 @app.route('/open_requests',methods=['GET','POST'])
 def open_requests():
@@ -205,35 +212,27 @@ def open_requests():
     if err:
         return render_template('login.html')
     user = users[emailsearch[email]]
-    return render_template('open_requests.html',requests=user.requests_made,itens=itens)   
+    return render_template('open_requests.html',requests=user.requests_made,itens=itens,users=users)   
 
 if __name__ == '__main__':
     programmers = [
-        Person(0,"joão", "henrrique", "99999999","xavier@gmail.com","bananeiras"),
-        Person(1,"guilherme","silva", "88888888","toledo@gmail.com","bahia")
+        save_user("joão", "henrrique", "99999999","xavier@gmail.com","bananeiras"),
+        save_user("guilherme","silva", "88888888","toledo@gmail.com","bahia")
     ]
     bananas = [
-        Item(0,"banana maçã",0),
-        Item(1,"banana nanica",0),
-        Item(2,"maçã",0),
-        Item(3,"banana prata",0),
-        Item(4,"banana nevada",1,desc="da gromis",photo="https://receitinhas.com.br/receita/pizza-de-banana-nevada/")
+        save_item("banana maçã",programmers[0],"não é uma maçã","https://st.focusedcollection.com/11312302/i/1800/focused_150226594-stock-photo-apple-and-yellow-banana.jpg",True),
+        save_item("banana nanica",programmers[0],"🤏","https://img.freepik.com/premium-photo/very-small-banana-hand_679905-1202.jpg?w=2000",True),
+        save_item("maça",programmers[0],"👩‍⚕️🏃‍♀️","🍏",False),
+        save_item("banana prata",programmers[0],"ingual o fredie mercury","prateado",True),
+        save_item("banana nevada",programmers[1],"da groomis","receitinhas.com.br/receita/pizza-de-banana-nevada/",True),
     ]
-    bananas[-1].comments["carlos"] = Comment("achei ruim\nbem ruim\nnão é lá essas coisas",2)
-    request_1 = Request('open',1,programmers[1].name,programmers[1].phone,programmers[0].phone)
-    request_2 = Request('accepted',4,programmers[0].name,programmers[0].phone,programmers[1].phone)
-    programmers[0].received_requests[(request_1.item,request_1.interested)] = request_1
-    programmers[1].requests_made[request_1.item] = request_1
-    programmers[1].received_requests[(request_2.item,request_2.interested)] = request_2
-    programmers[0].requests_made[request_2.item] = request_2
+    itens[bananas[-1]].comments["carlos"] = Comment("achei ruim\nbem ruim\nnão é lá essas coisas",2)
+    request_1 = Request('open',bananas[1],programmers[1],programmers[0])
+    request_2 = Request('accepted',bananas[4],programmers[0],programmers[1])
+    users[programmers[0]].received_requests[(request_1.item_id,request_1.interested)] = request_1
+    users[programmers[1]].requests_made[request_1.item_id] = request_1
+    users[programmers[1]].received_requests[(request_2.item_id,request_2.interested)] = request_2
+    users[programmers[0]].requests_made[request_2.item_id] = request_2
 
-    for p in programmers:
-        users[p.id] = p
-        emailsearch[p.email] = p.id
-
-    for b in bananas:
-        itens[b.id] = b
-        users[b.owner_id].itens[b.id] = b
-
-    engine = Engine(bananas)
+    engine = Engine(itens.values())
     app.run()
--- request.py
+++ request.py
@@ -1,10 +1,9 @@
 class Request:
-    def __init__(self,state:str,item:str,interested:str,interested_contact:str=None,supplier_contact:str=None):
+    def __init__(self,state:str,item_id:int,interested:int,supplier:int):
         self._state = state
-        self._item = item
+        self._item_id = item_id
         self._interested = interested
-        self._interested_contact = interested_contact
-        self._supplier_contact = supplier_contact
+        self._supplier = supplier
     
     @property
     def state(self)-> str:
@@ -17,19 +16,14 @@ class Request:
         return self._interested
     
     @property
-    def interested_contact(self)-> str:
+    def supplier(self)-> str:
         """get para o interested no request"""
-        return self._interested_contact
+        return self._supplier
     
     @property
-    def supplier_contact(self)-> str:
-        """get para o fornecedor no request"""
-        return self._supplier_contact
-    
-    @property
-    def item(self)-> str:
+    def item_id(self)-> int:
         """get para o id do item"""
-        return self._item
+        return self._item_id
     
     @state.setter
     def state(self,value:str)-> str:
